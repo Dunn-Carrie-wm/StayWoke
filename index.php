fb74d785e054c4edae2ec794ea5f4cffaf46bd5c
Start Coding Student 2 <session2@START-Coding-MBP-13.local>
2016-04-27 16:28:51 -0700

    Added header and footer with working links to the other pages.

--- index.php
+++ index.php
@@ -5,8 +5,38 @@
 <html>
 <head>
     <title>Stay Woke</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Stay Woke</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="">Home</a></li>
+                <li><a href="register/">Register</a></li>
+                <li><a href="#">Store</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="login/"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-out" >Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<footer>
+    <p style="background-color: black; color: white; text-align: center">
+        &copy; Stay Woke 2016
+    </p>
+</footer>
 </body>
 </html>
