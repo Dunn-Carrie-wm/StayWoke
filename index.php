004b7096e9dd12705d9ed1e073248569242b88a6
David <session2@START-Coding-MBP-21.local>
2016-04-26 17:02:40 -0700

    Added Navigation bar, Highscores page (unfilled), and Shop(unfilled)

--- index.php
+++ index.php
@@ -7,6 +7,23 @@
     <title>Stay Woke</title>
 </head>
 <body>
-
+    <div id="navbar" style="width: 700px; height: 50px;">
+        <a href="index.php"><div style="width: 25%; height: 50px; background-color: #ff5e07; float: left;">
+            <p style="float: left;">HOME</>
+            <div style="width: 1%; height: 50px; background-color: black; float: right"></div>
+        </div></a>
+        <a href="High%20Scores/index.php"><div style="width: 25%; height: 50px; background-color: #ff5e07; float: left;">
+            <p style="float: left;">HIGH-SCORES</>
+            <div style="width: 1%; height: 50px; background-color: black; float: right"></div>
+        </div></a>
+        <a href="login/index.php"><div style="width: 25%; height: 50px; background-color: #ff5e07; float: right">
+            <p style="float: left;">LOGIN/REGISTER</p>
+            <div style="width: 1%; height: 50px; background-color: black; float: right"></div>
+        </div></a>
+        <a href="Shop/index.php"><div style="width: 25%; height: 50px; background-color: #ff5e07; float: right">
+            <p style="float: left;">SHOP</>
+            <div style="width: 1%; height: 50px; background-color: black; float: right"></div>
+        </div></a>
+    </div>
 </body>
 </html>
